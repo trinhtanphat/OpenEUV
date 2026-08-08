7bb28e2b14129ed39ec480cb7c33252206349b09
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-08 13:10:44 +0700

    feat(education): integrate Assembly Explorer and learning path into atlas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { AssemblyExplorer } from './components/AssemblyExplorer'
 import { BuildMachine } from './components/BuildMachine'
 import { ContributorSection, FeatureStrip, Footer, Hero, Topbar } from './components/LayoutSections'
 import { EvidenceDashboard } from './components/EvidenceDashboard'
@@ -6,6 +7,7 @@ import { EvidenceGraph } from './components/EvidenceGraph'
 import { ExplorerSection } from './components/ExplorerSection'
 import { FabCaseStudies } from './components/FabCaseStudies'
 import { FabFlow } from './components/FabFlow'
+import { LearningPath } from './components/LearningPath'
 import { PatentExplorer } from './components/PatentExplorer'
 import { ResearchWorkbench } from './components/ResearchWorkbench'
 import { ResolutionSimulator } from './components/ResolutionSimulator'
@@ -21,6 +23,8 @@ export default function App() {
       <Hero language={language} />
       <ExplorerSection language={language} />
       <FeatureStrip />
+      <AssemblyExplorer language={language} />
+      <LearningPath language={language} />
       <ResearchWorkbench language={language} />
       <div className="two-col"><ResolutionSimulator /><BuildMachine /></div>
       <PatentExplorer />
