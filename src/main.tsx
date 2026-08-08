953b6c1aa58b32a5bcf488e8db4184a1d39a0920
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-08-08 12:02:37 +0700

    style(patent-v2): load patent coverage styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './advanced.css'
 import './depth.css'
 import './v2.css'
 import './v2-sim.css'
+import './v2-patent.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
